06c33bbeade24f97143300aa2d2257d0fe2eea1d
Olivier Kopala <s28526@pjwstk.edu.pl>
2024-03-12 11:28:54 +0100

    dodalem petle GetAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,3 +5,13 @@ Console.WriteLine("and here comes sebastian vettel");
 Console.WriteLine("and lando norris is on pole");
 Console.WriteLine("soy lago");
 
+static double GetAverage(int[] tab)
+{
+    int sum = 0;
+    foreach (var VARIABLE in tab)
+    {
+        sum += VARIABLE;
+    }
+
+    return (double)sum / tab.Length;
+}
